f2713ccf2a95e7a9f9c5c8ed7455768ed5761450
Liang Dong <liangdong@cpp.edu>
2017-02-09 23:33:15 -0800

    InsertionSort v1.2

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,40 +2,76 @@ package insertionSort;
 
 import java.util.Random;
 
+import lab1.ArrayBag;
+
 public class InsertionSort {
 
 	public static void main(String[] args) {
+		ArrayBag<Integer> a1 = new ArrayBag<>(1000);
+		ArrayBag<Integer> a2 = new ArrayBag<>(1000);
+
 		Random r = new Random();
-		int[] array =new int[10];
-		System.out.println("random integers: ");
+		int[] array =new int[1000];
+		
 		for(int i = 0; i < array.length; i++) {
-			array[i] = r.nextInt(50);
-		}
-		for(int i = 0; i < array.length; i++) {
-			 System.out.println(array[i]);
-		}
-		System.out.println("insertionSortByRecursion: ");
-		insertionSortByRecursion(array,0);
-		for(int i = 0; i < array.length; i++) {
-			 System.out.println(array[i]);
-		}
-		System.out.println("random integers: ");
-		for(int i = 0; i < array.length; i++) {
-			array[i] = r.nextInt(50);
-		}
-		for(int i = 0; i < array.length; i++) {
-			 System.out.println(array[i]);
+			array[i] = r.nextInt(1000);
+			a1.add(array[i]);
+			//System.out.println(array[i]);
 		}
 		System.out.println("insertionSortByIteration: ");
 		insertionSortByIteration(array);
 		for(int i = 0; i < array.length; i++) {
-			 System.out.println(array[i]);
+			a2.add(array[i]);
+			//System.out.println(array[i]);
+		}
+		boolean pass = true;
+		for(int i = 0; i < array.length - 1; i++) {
+			if(array[i] > array[i + 1]) {
+				System.out.println("*****Failed");
+				pass = false;
+				break;
+			}
 		}
 		
+		if(a1.equals(a2) && pass) {
+			System.out.println("Sorted.");
+		}
+		System.out.println();
 		
+		
+		
+		a1.clear();
+		a2.clear();
+
+		for(int i = 0; i < array.length; i++) {
+			array[i] = r.nextInt(500);
+			a1.add(array[i]);
+			//System.out.println(array[i]);
+		}
+		System.out.println("insertionSortByRecursion: ");
+		insertionSortByRecursion(array,0);
+		for(int i = 0; i < array.length; i++) {
+			a2.add(array[i]);
+			//System.out.println(array[i]);
+		}
+		pass = true;
+		for(int i = 0; i < array.length - 1; i++) {
+			if(array[i] > array[i + 1]) {
+				System.out.println("*****Failed");
+				pass = false;
+				break;
+			}
+		}
+		if(a1.equals(a2) && pass) {
+			System.out.println("Sorted.");
+		}
 
 	}
 	
+	/**
+	 * a function that uses iterative insertion sort to sort an array.
+	 * @param array The array that to be sorted
+	 */
 	public static void insertionSortByIteration (int[] array) {
 		for (int i = 0; i < array.length; i++) {
 			int j = i;
@@ -48,6 +84,12 @@ public class InsertionSort {
 		}//end for
 	}//end  insertionSortByIteration
 	
+	
+	/**
+	 * a function that uses recursive insertion sort to sort an array.
+	 * @param array The array that to be sorted
+	 * @param firstUnsorted The index of the first entry in an unsorted part of the array
+	 */
 	public static void insertionSortByRecursion (int[] array, int firstUnsorted) {
 		
 		movingElement(array, firstUnsorted);	
@@ -57,7 +99,13 @@ public class InsertionSort {
 		
 	}//end insertionSortByRecursion
 	
-	public static void movingElement (int[] array, int indexOfChekingElement) {
+	
+	/**
+	 * compare and swap entries when necessary.
+	 * @param array The target array
+	 * @param indexOfChekingElement Index of the entry that is being checked
+	 */
+	private static void movingElement (int[] array, int indexOfChekingElement) {
 		
 		if((indexOfChekingElement <= 0) 
 				|| (array[indexOfChekingElement] >= array[indexOfChekingElement - 1]))
@@ -68,7 +116,6 @@ public class InsertionSort {
 			array[indexOfChekingElement - 1] = temp;
 			movingElement(array, indexOfChekingElement - 1);
 		}//end else
-		
 		
 	}//end comparingElements
 	
